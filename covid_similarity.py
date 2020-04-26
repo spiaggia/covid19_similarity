45461b1dcbf5fe42edecee744b4992cfe2ff20ba
Yu Hamada <yu_hamada@so-netmedia.jp>
2020-04-26 16:37:10 +0900

    sort by dist

--- covid_similarity.py
+++ covid_similarity.py
@@ -1,5 +1,18 @@
 import Levenshtein
+import pandas as pd
 
+
+def pick_place_name(s):
+  place = s
+  if "Wuhan" in s:
+    place = "Wuhan"
+  if "TWN" in s:
+    place = "TWN"
+  if "USA" in s:
+    place = "USA"
+  return place
+
+df = pd.DataFrame(columns=['place1', 'place2', 'dist'])
 
 path = 'sequences.fasta'
 
@@ -17,31 +30,42 @@ f.close()
 covid_nucleic_acid_sequence_array = s.split('>')
 
 covid_nucleic_acid_sequence_array_length = len(covid_nucleic_acid_sequence_array)
-for covid_nucleic_acid_sequence in covid_nucleic_acid_sequence_array:
-#  print(covid_nucleic_acid_sequence_array_length)
-  str_array1 = covid_nucleic_acid_sequence_array[1].splitlines()[1:]
 
-  str1 = ""
+for num1 in range(1,10):
+  covid_nucleic_acid_sequence_data1 = covid_nucleic_acid_sequence_array[num1]
+  covid_nucleic_acid_sequence_definition1 = covid_nucleic_acid_sequence_data1.splitlines()[0]
+  str_array1 = covid_nucleic_acid_sequence_data1.splitlines()[1:]
+
+  covid_nucleic_acid_sequence1 = ""
   for x in str_array1:
-      str1 += x
+      covid_nucleic_acid_sequence1 += x
 
-#  print(str1)
-
-  str_array2 = covid_nucleic_acid_sequence.splitlines()[1:]
-
-  str2 = ""
-  for x in str_array2:
-      str2 += x
-
-#  print(str2)
+#  print(covid_nucleic_acid_sequence1)
+  for num2 in range(1,10):
+    covid_nucleic_acid_sequence_data2 = covid_nucleic_acid_sequence_array[num2]
+    covid_nucleic_acid_sequence_definition2 = covid_nucleic_acid_sequence_data2.splitlines()[0]
+    str_array2 = covid_nucleic_acid_sequence_data2.splitlines()[1:]
 
 
-  lev_dist = Levenshtein.distance(str1, str2)
-  print(lev_dist)
+    covid_nucleic_acid_sequence2 = ""
+    for x in str_array2:
+        covid_nucleic_acid_sequence2 += x
 
-  divider = len(str1) if len(str1) > len(str2) else len(str2)
-  lev_dist = lev_dist / divider
-  lev_dist = 1 - lev_dist
+  #  print(covid_nucleic_acid_sequence2)
+
+    if num1 != num2:
+      lev_dist = Levenshtein.distance(covid_nucleic_acid_sequence1,covid_nucleic_acid_sequence2)
+      print()
+      place1 = pick_place_name(covid_nucleic_acid_sequence_definition1)
+      place2 = pick_place_name(covid_nucleic_acid_sequence_definition2)
+
+      divider = len(covid_nucleic_acid_sequence1) if len(covid_nucleic_acid_sequence1) > len(covid_nucleic_acid_sequence2) else len(covid_nucleic_acid_sequence2)
+      lev_dist = lev_dist / divider
+      lev_dist = 1 - lev_dist
+      print(lev_dist)
+      s = pd.Series([place1,place2,lev_dist], index=df.columns)
+      print(s)
+      df = df.append(s, ignore_index=True )
+print(df.sort_values('dist'))
 
 
-  print(lev_dist)
